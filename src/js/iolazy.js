80e391b672d4c993bfccbaa65417175c30802edd
cdowdy <cdowdy46@gmail.com>
2017-08-08 16:20:31 -0400

    adds source file for iolazy

---
+++ src/js/iolazy.js
@@ -0,0 +1,67 @@
+// PolyFill for "isIntersecting"
+// https://github.com/WICG/IntersectionObserver/issues/211#issuecomment-309144669
+if ('IntersectionObserver' in window &&
+    'IntersectionObserverEntry' in window &&
+    'intersectionRatio' in window.IntersectionObserverEntry.prototype &&
+    !('isIntersecting' in IntersectionObserverEntry.prototype)) {
+
+    Object.defineProperty(window.IntersectionObserverEntry.prototype, 'isIntersecting', {
+        get: function () {
+            return this.intersectionRatio > 0
+        }
+    })
+}
+
+// another for nodelist.foreach()
+// https://developer.mozilla.org/en-US/docs/Web/API/NodeList/forEach#Polyfill
+if (window.NodeList && !NodeList.prototype.forEach) {
+    NodeList.prototype.forEach = function (callback, thisArg) {
+        thisArg = thisArg || window;
+        for (var i = 0; i < this.length; i++) {
+            callback.call(thisArg, this[i], i, this);
+        }
+    };
+}
+
+class IOlazy {
+
+    constructor( { image = '.lazyload', threshold = .006 } = {} ) {
+
+        this.threshold = threshold;
+        this.image = document.querySelectorAll(image);
+        // the intersection observer
+        this.observer = new IntersectionObserver( ::this.handleChange, {
+            threshold: [ this.threshold ]
+        });
+
+        this.lazyLoad();
+    }
+
+    handleChange(changes) {
+
+        changes.forEach(change => {
+
+            if (change.isIntersecting) {
+
+                change.target.classList.add('visible');
+
+                if ( change.target.dataset.srcset ) {
+                    change.target.srcset = change.target.dataset.srcset;
+                }
+
+                if (change.target.dataset.src) {
+                    change.target.src = change.target.dataset.src;
+                }
+
+                this.observer.unobserve(change.target);
+            }
+        });
+    }
+
+    lazyLoad() {
+
+        this.image.forEach( img => {
+            this.observer.observe(img);
+        })
+    }
+}
